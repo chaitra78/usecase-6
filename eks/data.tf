f0287f8cd14f348170839737920c2f3d92daa677
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-15 15:29:58 +0530

    Update data.tf

--- eks/data.tf
+++ eks/data.tf
@@ -16,8 +16,3 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
-
-data "aws_key_pair" "project" {
-  key_name = "project"
-}
-
